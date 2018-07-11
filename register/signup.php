c5113485c37dc1db3ab6fd71d68b7d6924ca4a19
yoshi4363 <yoshi4363@gmail.com>
2018-07-11 17:20:09 +0900

    チェック画面から戻ってきた際の処理

--- register/signup.php
+++ register/signup.php
@@ -3,6 +3,17 @@
 
     // $errors[] = ""; ではダメ 
     $errors = array();
+    $name = "";
+    $email = "";
+
+    // チェック画面から戻ってきた際の処理
+    if (isset($_GET["action"]) && $_GET["action"] == "rewrite") {
+        $_POST["input_name"] = $_SESSION["register"]["name"];
+        $_POST["input_email"] = $_SESSION["register"]["email"];
+        $_POST["input_password"] = $_SESSION["register"]["password"];
+
+        $errors["rewrite"] = "true";
+    }
 
     if (!empty($_POST)) {
         $name = $_POST["input_name"];
@@ -26,11 +37,14 @@
             $errors["password"] = "length"; 
         }
 
+        $file_name = "";
         // 「file」は$_POSTで受け取れない（$_FILESを使う）
         // $_FILES["キー"]["name"]; で画像名取得
         // $_FILES["キー"]["tmp_name"]; 画像そのものを取得（テンポラリーファイル）
         // 画像取得ルール。inputタグにてtype = "file" になっている、formタグにてenctype属性にmultipart/form-dataが指定されている、POST送信になっている。
-        $file_name = $_FILES["input_img_name"]["name"];
+        if (!isset($_GET["action"])) {
+            $file_name = $_FILES["input_img_name"]["name"];
+        }
         if (!empty($file_name)) {
             // substr関数：第一引数から文字列を取得
             $file_type = substr($file_name, -4);
@@ -58,10 +72,10 @@
             move_uploaded_file($_FILES["input_img_name"]["tmp_name"], "../user_profile_img/" . $submit_file_name);
 
             // SESSIONはサーバー内全てで共通しているため、重複を防ぐため多次元配列化（registerキーを設置）
-            $_SESSION["register"]["name"] = $_POST["input_name"];
-            $_SESSION["register"]["email"] = $_POST["input_email"];
+            $_SESSION["register"]["name"] = $name;
+            $_SESSION["register"]["email"] = $email;
             $_SESSION
-            ["register"]["password"] = $_POST["input_password"];
+            ["register"]["password"] = $password;
             $_SESSION["register"]["img_name"] = $submit_file_name;
 
             header("Location: check.php");
@@ -87,7 +101,7 @@
           <div class="form-group">
             <!-- forの値とid属性の値を同じにして紐付け -->
             <label for="name">ユーザー名</label>
-            <input type="text" name="input_name" class="form-control" id="name" placeholder="山田 太郎">
+            <input type="text" name="input_name" class="form-control" id="name" placeholder="山田 太郎" value="<?php echo htmlspecialchars($name); ?>">
             <!-- isset：変数に値がセットされているかつNULLでないことを調べる -->
             <?php if (isset($errors["name"]) && $errors["name"] == "blank") { ?>
               <p class="text-danger">ユーザー名を入力して下さい。</p>
@@ -95,7 +109,7 @@
           </div>
           <div class="form-group">
             <label for="email">メールアドレス</label>
-            <input type="email" name="input_email" class="form-control" id="email" placeholder="example@gmail.com">
+            <input type="email" name="input_email" class="form-control" id="email" placeholder="example@gmail.com" value="<?php echo htmlspecialchars($email); ?>">
             <?php if (isset($errors["email"]) && $errors["email"] == "blank") { ?>
               <p class="text-danger">メールアドレスを入力して下さい。</p>
             <?php } ?>
@@ -108,6 +122,9 @@
             <?php } ?>
             <?php if (isset($errors["password"]) && $errors["password"] == "length") { ?>
               <p class="text-danger">パスワードは４〜１６文字で入力して下さい。</p>
+            <?php } ?>
+            <?php if (isset($errors["rewrite"]) && $errors["rewrite"] == "true") { ?>
+              <p class="text-danger">パスワードを再度入力して下さい。</p>
             <?php } ?>
           </div>
           <div class="form-group">
